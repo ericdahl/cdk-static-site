dee9d999454c19593b324baa3ab9ad6cb2f3e948
Eric Dahl <eric.c.dahl@gmail.com>
2024-04-14 11:05:45 -0700

    add site stuff

--- cdk_static_site/cdk_static_site_stack.py
+++ cdk_static_site/cdk_static_site_stack.py
@@ -1,19 +1,48 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_s3_deployment as s3_deploy,
+    aws_cloudfront as cloudfront,
+    aws_cloudfront_origins as cloudfront_origins,
 )
 from constructs import Construct
+
 
 class CdkStaticSiteStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        s3_access_logs = s3.Bucket(
+            self, "S3AccessLogs"
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkStaticSiteQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = s3.Bucket(
+            self, "CdkStaticSiteBucket",
+            server_access_logs_bucket=s3_access_logs
+        )
+
+        s3_deploy.BucketDeployment(
+            self, "CdkStaticSiteDeployment",
+            sources=[s3_deploy.Source.asset("./site")],
+            destination_bucket=bucket
+        )
+
+        cloudfront.Distribution(
+            self, "CdkStaticSiteDistribution",
+            default_behavior=cloudfront.BehaviorOptions(
+                # note: this creates an OAI implicitly. CDK doesn't support OAC.
+                # https://github.com/aws/aws-cdk/issues/21771
+                origin=cloudfront_origins.S3Origin(bucket=bucket),
+                compress=True,
+            ),
+            default_root_object="index.html",
+
+            error_responses=[
+                cloudfront.ErrorResponse(
+                    http_status=404,
+                    response_page_path="/error/404.html")
+            ],
+            # implicitly creates a S3 bucket for logging and adds ACL for awslogsdelivery to it
+            enable_logging=True
+        )
